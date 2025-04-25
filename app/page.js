56e9dafe38cb98a9fcc13be8777fab609d2820ff
jordan-frampton <jordan.frampton@flybreeze.com>
2025-04-25 14:32:33 -0600

    Update layout and spacing in Home component

--- app/page.js
+++ app/page.js
@@ -5,11 +5,11 @@ import EarlHorseLogo from "../public/images/EarlHorse.svg";
 
 export default function Home() {
     return (
-        <div className={"flex flex-col h-dvh w-full md:justify-center items-center text-[#FFFD00] font-bb-modern relative pt-[33vh] md:pt-0"}>
+        <div className={"flex flex-col h-dvh w-full md:justify-center items-center text-[#FFFD00] font-bb-modern relative"}>
             <div className={'absolute min-w-[200px] md:min-w-[300px] top-20 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-1000 ease-in-out'}>
                 <Image src={EarlPantryLogo} alt={'Earl Horse'} width={300} height={300}/>
             </div>
-            <div className={"coming-soon-content flex flex-col md:flex-row transition-all duration-1000 ease-in-out"}>
+            <div className={"coming-soon-content flex flex-col md:flex-row transition-all duration-1000 ease-in-out mt-[250px] md:mt-0"}>
                 <div className={'self-center md:px-0 md:mr-4 w-[225px] lg:w-[300px] xl:w-[350px]'}>
                     <Image src={EarlHorseLogo} alt={'Earl Horse'} width={350} height={350}/>
                 </div>
